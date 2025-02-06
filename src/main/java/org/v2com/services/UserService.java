63dca2498c2478cc3c3397f30270b12b5f17aab4
jeziel <jezielnogueira.15@gmail.com>
2025-02-06 14:47:11 -0300

    user more refactoring

--- src/main/java/org/v2com/services/UserService.java
+++ src/main/java/org/v2com/services/UserService.java
@@ -1,9 +1,11 @@
 package org.v2com.services;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.v2com.entities.UserEntity;
+import org.v2com.repositories.UserRepository;
 
 import java.util.List;
 import java.util.UUID;
@@ -15,44 +17,42 @@ import static jakarta.transaction.Transactional.TxType.SUPPORTS;
 @Transactional(REQUIRED)
 public class UserService {
 
+    @Inject
+    UserRepository userRepository;
+
 
     public List<UserEntity> findAllUsers() {
-        return UserEntity.listAll();
+        return userRepository.getAllUsers();
     }
 
     public UserEntity findUserById(long id) {
-        return UserEntity.findById(id);
-    }
-
-    public UserEntity findUserByEmail(String email) {
-        return UserEntity.find("email", email).firstResult();
+        return userRepository.findById(id);
     }
 
     public UserEntity finUserByName(String name){
-        return UserEntity.find("name", name).firstResult();
+        return userRepository.findByName(name);
     }
 
     @Transactional(SUPPORTS)
-    public UserEntity addUser(@Valid UserEntity userEntity) {
-        userEntity.persist();
-        return userEntity;
+    public void addUser(@Valid UserEntity userEntity) {
+        userRepository.persist(userEntity);
     }
 
     @Transactional(SUPPORTS)
     public void deleteUser(long id) {
-        UserEntity.deleteById(id);
+        userRepository.deleteById(id);
     }
 
     @Transactional(SUPPORTS)
     public UserEntity updateUser(UUID id, @Valid UserEntity userEntity) {
-        UserEntity existingUser = UserEntity.findById(id);
+        UserEntity existingUser = userRepository.findbyId(id);
 
-        existingUser.name = userEntity.name;
-        existingUser.address = userEntity.address;
-        existingUser.email = userEntity.email;
-        existingUser.phone = userEntity.phone;
+        existingUser.setName(userEntity.getName());
+        existingUser.setAddress(userEntity.getAddress());
+        existingUser.setEmail(userEntity.getEmail());
+        existingUser.setPhone(userEntity.getPhone());
 
-        existingUser.persist();
+        userRepository.persist(existingUser);
         return existingUser;
     }
 
